663a887e43d9641efc806f8d473f0aa92d3bd05c
Elmedin Turkeš <elmthedev@gmail.com>
2020-02-18 13:46:31 +0100

    Update index.cpp

--- index.cpp
+++ index.cpp
@@ -4,10 +4,10 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const byte ROWS = 4; //four rows
-const byte COLS = 4; //four columns
-// Define the Keymap
-char hexaKeys[ROWS][COLS] = {
+const byte ROWS = 4;
+const byte COLS = 4;
+
+const char hexaKeys[ROWS][COLS] = {
   {
     '1',
     '2',
@@ -33,103 +33,76 @@ char hexaKeys[ROWS][COLS] = {
     'D'
   }
 };
-byte rowPins[ROWS] = {
+const byte rowPins[ROWS] = {
   9,
   8,
   7,
   6
-}; //connect to the Rows of the keypad pin 8, 7, 6, 5 respectively
-byte colPins[COLS] = {
+};
+const byte colPins[COLS] = {
   5,
   4,
   3,
   2
-}; //connect to the Columns of the keypad pin 4, 3, 2, 1 respectively
+};
+const Keypad tastatura = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
 
-//initialize an instance of class NewKeypad
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+const String lozinke[3] = {
+  "5394",
+  "0000",
+  "1234"
+};
+int racuni[3] = {
+  13000,
+  24,
+  -6
+};
 
-int PASSWORD1 = "5394";
-int PASSWORD2 = "0000";
-int PASSWORD3 = "1234";
-
-int RACUN1 = 13000;
-int RACUN2 = 24;
-int RACUN3 = -6;
-
+String zaIsplatu = "";
+String sifra = "";
 bool done = false;
 bool finished = false;
 bool biranje = false;
 bool isplacuje = false;
-int brojac = 0;
-String zaIsplatu = "";
 int racun = 0;
-String sifra = "";
+int brojac = 0;
 
 void setup() {
   Serial.begin(9600);
+
   lcd.init();
-  // turn on the backlight
   lcd.backlight();
+
   resetuj();
 }
 
+// Provjerava da li postoji račun i da li je unešena šifra ispravna
 bool verifikuj(int racun, String sifra) {
-  switch (racun) {
-  case 1:
-    {
-      return sifra == PASSWORD1;
-    }
-  case 2:
-    {
-      return sifra == PASSWORD2;
-    }
-  case 3:
-    {
-      return sifra == PASSWORD3;
-    }
-  default:
-    {
-      return false;
-    };
-  }
+  if (racun < 1 || racun > 3)
+    return false;
+
+  return lozinke[racun - 1] == sifra;
 }
 
+// Oduzima novac sa računa i vraća novo stanje
 int stanje(int racun, int novoStanje = 0) {
+  if (racun < 1 || racun > 3)
+    return -1;
 
-  switch (racun) {
-  case 1:
-    {
-      RACUN1 -= novoStanje;
-      return RACUN1;
-    }
-  case 2:
-    {
-      RACUN2 -= novoStanje;
-      return RACUN2;
-    }
-  case 3:
-    {
-      RACUN3 -= novoStanje;
-      return RACUN3;
-    }
-  default:
-    {
-      return false;
-    };
-  }
-
+  racuni[racun - 1] -= novoStanje;
+  return racuni[racun - 1];
 }
 
+// Vraća sve vrijednosti na početnu vrijednost
 void resetuj() {
-  sifra = "";
-  brojac = 0;
-  racun = 0;
-  biranje = false;
-  done = false;
   zaIsplatu = "";
+  sifra = "";
+  done = false;
   finished = false;
+  biranje = false;
   isplacuje = false;
+  racun = 0;
+  brojac = 0;
 
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -138,120 +111,113 @@ void resetuj() {
   lcd.print("racuna:");
 }
 
-int multiplier = 1;
-void loop() {
-  char unos = customKeypad.getKey();
+void isplacivanje(char unos) {
+  if (unos != 'A' && unos != 'B' && unos != 'C' && unos != '*' && unos != '#' && unos != 'D') {
+    zaIsplatu += unos;
 
-  if (unos) {
-    if (isplacuje) {
-      if(unos != 'A' && unos != 'B' && unos != 'C' && unos != '*' && unos != '#' && unos != 'D') {
-          zaIsplatu += unos;
-          
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Isplatiti: ");
-          lcd.setCursor(0, 1);
-          lcd.print(zaIsplatu);
-      } else if(unos == 'D') {
-        int stanjeRac = stanje(racun, 0);
-          if(zaIsplatu.toInt() % 10 == 0 && zaIsplatu.toInt() > 0 && stanjeRac >= zaIsplatu.toInt()  ) {
-             stanje(racun, zaIsplatu.toInt());
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("Isplaceno.");
-            isplacuje = false;
-            delay(5000);
-            resetuj();
-          } else {
-            if(stanjeRac < zaIsplatu.toInt()) {
-              lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("Nema dovoljno");
-            lcd.setCursor(0, 1);
-            lcd.print("sredstava.");
-            delay(5000);
-            resetuj();
-            } else {
-            lcd.clear();
-            lcd.setCursor(0, 0);
-            lcd.print("Pogresan unos.");  
-            delay(5000);
-            resetuj();
-            }
-            
-          }
-      }
-    } else if (biranje) {
-      if (unos == 'D' || (done && unos == 'D') || (finished && unos == 'D')) {
-        resetuj();
-        return;
-      }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Isplatiti: ");
+    lcd.setCursor(0, 1);
+    lcd.print(zaIsplatu);
+  } else if (unos == 'D') {
+    int stanjeRac = stanje(racun, 0);
 
-      if (done && unos == 'A') {
-        finished = true;
-        int stanjeRac = stanje(racun, 0);
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Racun ");
-        lcd.print(racun);
-        lcd.setCursor(0, 1);
-        lcd.print("BAM ");
-        lcd.print(stanjeRac);
-        return;
-      } else if (done && unos == 'B') {
-        //finished = true;
-        //int stanjeRac = stanje(racun, 0);
-        isplacuje = true;
-        done = true;
-        lcd.clear();
-        lcd.setCursor(0, 0);
-        lcd.print("Isplatiti: ");
-        lcd.setCursor(0, 1);
-        lcd.print(zaIsplatu);
-        return;
-      }
+    if (zaIsplatu.toInt() % 10 == 0 && zaIsplatu.toInt() > 0 && stanjeRac >= zaIsplatu.toInt()) {
+      stanje(racun, zaIsplatu.toInt());
 
-      brojac++;
-      sifra += unos;
       lcd.clear();
       lcd.setCursor(0, 0);
-      lcd.print("Unesite sifru:");
+      lcd.print("Isplaceno.");
+
+      isplacuje = false;
+
+      delay(5000);
+      resetuj();
+    } else {
+      if (stanjeRac < zaIsplatu.toInt()) {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Nema dovoljno");
+        lcd.setCursor(0, 1);
+        lcd.print("sredstava.");
+
+        delay(5000);
+        resetuj();
+      } else {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Pogresan unos.");
+
+        delay(5000);
+        resetuj();
+      }
+    }
+  }
+}
+
+void meni(char unos) {
+  if (unos == 'D' || (done && unos == 'D') || (finished && unos == 'D')) {
+    resetuj();
+    return;
+  }
+
+  if (done && unos == 'A') {
+    finished = true;
+    int stanjeRac = stanje(racun, 0);
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Racun ");
+    lcd.print(racun);
+    lcd.setCursor(0, 1);
+    lcd.print("BAM ");
+    lcd.print(stanjeRac);
+    return;
+  } else if (done && unos == 'B') {
+    isplacuje = true;
+    done = true;
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Isplatiti: ");
+    lcd.setCursor(0, 1);
+    lcd.print(zaIsplatu);
+    return;
+  }
+
+  brojac++;
+  sifra += unos;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Unesite sifru:");
+  lcd.setCursor(0, 1);
+  lcd.print(sifra);
+  if (brojac == 4) {
+    if (verifikuj(racun, sifra)) {
+      int stanjeRac = stanje(racun, 0);
+
+      done = true;
+
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("A - Stanje");
+      lcd.setCursor(0, 1);
+      lcd.print("B - Isplata");
+    } else {
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Pogresna sifra");
       lcd.setCursor(0, 1);
       lcd.print(sifra);
-      if (brojac == 4) {
-        Serial.println(sifra);
-        if (verifikuj(racun, sifra)) {
-          int stanjeRac = stanje(racun, 0);
-          done = true;
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("A - Stanje");
-          lcd.setCursor(0, 1);
-          lcd.print("B - Isplata");
-          /*
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Racun ");
-          lcd.print(racun);
-          lcd.setCursor(0, 1);
-          lcd.print("BAM ");
-          lcd.print(stanjeRac); */
+    }
 
-        } else {
-          Serial.println("Pogresna sifra");
-          lcd.clear();
-          lcd.setCursor(0, 0);
-          lcd.print("Pogresna sifra");
-          lcd.setCursor(0, 1);
-          lcd.print(sifra);
-          //resetuj();
-        }
+  }
+}
 
-      }
-    } else {
-      if (unos != '1' && unos != '2' && unos != '3') {
-        Serial.println("Ne postoji racun");
-
+void unosenje(char unos) {
+    if (unos != '1' && unos != '2' && unos != '3') {
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Ne postoji!");
@@ -265,22 +231,26 @@ void loop() {
         lcd.setCursor(0, 0);
         lcd.print("Unesite sifru:");
         lcd.setCursor(0, 1);
-        //lcd.print("racuna:");
-        Serial.println("Unesite sifru: ");
 
         if (unos == '1')
           racun = 1;
-
-        if (unos == '2')
+        else if (unos == '2')
           racun = 2;
-
-        if (unos == '3')
+        else if (unos == '3')
           racun = 3;
-
-        if (unos == '4')
-          racun = 4;
-
       }
+}
+
+void loop() {
+  char unos = tastatura.getKey();
+
+  if (unos) {
+    if (isplacuje) {
+      isplacivanje(unos);
+    } else if (biranje) {
+        meni(unos);
+    } else {
+      unosenje();
     }
   }
 }
